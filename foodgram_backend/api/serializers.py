5299f37a3744226fc76a1b2d2e15c39fcd7f497f
Iskander <hazik383@gmail.com>
2023-09-25 00:47:03 +0500

    To deploy

--- foodgram_backend/api/serializers.py
+++ foodgram_backend/api/serializers.py
@@ -119,7 +119,7 @@ class RecipeGetSerializer(serializers.ModelSerializer):
     )
     is_favorited = serializers.SerializerMethodField(read_only=True)
     is_in_shopping_cart = serializers.SerializerMethodField(read_only=True)
-    image = Base64ImageField(source='image.url')
+    image = Base64ImageField()
 
     class Meta:
         model = Recipe
